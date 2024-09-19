0f74da55aa881f0a26ca50eee686d1498596cbe5
LAB34 <lab34@fatec.com>
2024-09-19 19:22:27 -0300

    Commit inicial

---
+++ src/main/java/com/receitasapi/receitasapi/ReceitasapiApplication.java
@@ -0,0 +1,13 @@
+package com.receitasapi.receitasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceitasapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceitasapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/receitasapi/receitasapi/ReceitasapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.receitasapi.receitasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitasapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
